12552b9585b426be5e48795906d4e41ab7104c06
Victor Corte <victorcortelc@gmail.com>
2021-03-07 16:09:35 -0300

    future consumption

--- lib/portfolio/screen/portfolio.dart
+++ lib/portfolio/screen/portfolio.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:goatfolio/authentication/service/cognito.dart';
 import 'package:goatfolio/common/widget/cupertino_sliver_page.dart';
 import 'package:goatfolio/performance/client/performance_client.dart';
+import 'package:goatfolio/performance/model/monthly_performance.dart';
 import 'package:goatfolio/portfolio/widget/donut_chart.dart';
 import 'package:provider/provider.dart';
 
@@ -19,35 +20,105 @@ class PortfolioPage extends StatefulWidget {
 
 class _PortfolioPageState extends State<PortfolioPage> {
   Map<String, Rgb> colors = Map();
+  Future<List<StockMonthlyPerformance>> _future;
+  PerformanceClient client;
 
   @override
   void initState() {
     super.initState();
     final userService = Provider.of<UserService>(context, listen: false);
-    final client = PerformanceClient(userService);
-    client.getPerformance();
+    client = PerformanceClient(userService);
+    _future = client.getPerformance();
   }
+
+  //TODO getAll
 
   @override
   Widget build(BuildContext context) {
     return CupertinoSliverPage(largeTitle: PortfolioPage.title, children: [
-      Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            alignment: Alignment.centerLeft,
-            padding: EdgeInsets.only(left: 16, bottom: 16, top: 8),
-            child: Text(
-              "Alocação",
-              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-            ),
-          ),
-          SizedBox(
-              height: 240,
-              width: double.infinity,
-              child: DonutAutoLabelChart(buidInvestmentSeries()))
-        ],
-       ),
+      FutureBuilder(
+        future: _future,
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+            case ConnectionState.active:
+              break;
+            case ConnectionState.waiting:
+              return CupertinoActivityIndicator();
+            case ConnectionState.done:
+              if (snapshot.hasData) {
+                final List<StockMonthlyPerformance> monthlyPerformances = snapshot.data;
+                return Column(
+                  children: [
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      padding: EdgeInsets.only(left: 16, bottom: 16, top: 8),
+                      child: Text(
+                        "Alocação",
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 240,
+                      width: double.infinity,
+                      child: DonutAutoLabelChart(buidInvestmentSeries(),),),
+                    Container(
+                      padding: EdgeInsets.only(left: 16, right: 16),
+                      child: ListView.builder(
+                        padding: EdgeInsets.zero,
+                        physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: monthlyPerformances.length,
+                        itemBuilder: (context, index) {
+                          return Text(monthlyPerformances[index].ticker);
+                        },
+                      ),
+                    ),
+                  ],
+                );
+              }
+          }
+          return Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: 32,
+              ),
+              Text("Tivemos um problema ao carregar",
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .subtitle1),
+              Text(" as informações.",
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .subtitle1),
+              SizedBox(
+                height: 8,
+              ),
+              Text("Toque para tentar novamente.",
+                  style: Theme
+                      .of(context)
+                      .textTheme
+                      .subtitle1),
+              CupertinoButton(
+                padding: EdgeInsets.all(0),
+                child: Icon(
+                  Icons.refresh_outlined,
+                  size: 32,
+                ),
+                onPressed: () {
+                  setState(() {
+                    _future = client.getPerformance();
+                  });
+                },
+              ),
+            ],
+          );
+        },
+      ),
     ]);
   }
 
@@ -64,11 +135,12 @@ class _PortfolioPageState extends State<PortfolioPage> {
         domainFn: (TickerTotals totals, _) => totals.ticker,
         measureFn: (TickerTotals totals, _) => totals.total,
         data: data,
-        colorFn: (totals, _) => charts.Color(
-            r: totals.color.r, g: totals.color.g, b: totals.color.b),
+        colorFn: (totals, _) =>
+            charts.Color(
+                r: totals.color.r, g: totals.color.g, b: totals.color.b),
         // Set a label accessor to control the text of the arc label.
         labelAccessorFn: (TickerTotals totals, _) =>
-            '${totals.ticker.replaceAll('.SA', '')}',
+        '${totals.ticker.replaceAll('.SA', '')}',
       )
     ];
   }
