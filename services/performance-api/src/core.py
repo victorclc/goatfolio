89b98a25c4465f971ede9d5b6dd426a10b37a41d
Victor Corte <victorcortelc@gmail.com>
2021-03-18 01:01:33 -0300

    separating stocks from reits

--- services/performance-api/src/core.py
+++ services/performance-api/src/core.py
@@ -106,53 +106,23 @@ class PerformanceCore:
             prev = proc
             proc = proc + relativedelta(months=1)
 
-    def consolidate_portfolio_history(self, portfolio, investment):
-        history_dict = {int(h.date.timestamp()): h for h in portfolio.history}
-        value = investment.amount * investment.price * (-1 if investment.operation == OperationType.SELL else 1)
-        month_date = DatetimeUtils.month_first_day_datetime(investment.date)
-        month_timestamp = int(month_date.timestamp())
-
-        if month_timestamp not in history_dict:
-            prev_month_timestamp = int(
-                DatetimeUtils.month_first_day_datetime(investment.date - relativedelta(months=1)).timestamp())
-            position = PortfolioPosition(date=month_date)
-            history_dict[month_timestamp] = position
-            portfolio.history.append(position)
-
-            self._fix_portfolio_history_gap(portfolio.history, history_dict, DatetimeUtils.month_first_day_datetime(
-                datetime.now()))
-
-            if prev_month_timestamp in history_dict:
-                position.total_invested = position.total_invested + history_dict[prev_month_timestamp].total_invested
-
-        for timestamp in list(filter(lambda d: d >= month_timestamp, history_dict.keys())):
-            print(f"Updating history in timestamp: {timestamp}")
-            history_dict[timestamp].total_invested = history_dict[timestamp].total_invested + value
-
-    def consolidate_stock(self, portfolio: Portfolio, investment: StockInvestment):
-        stocks_dict = {s.ticker: s for s in portfolio.stocks + portfolio.reits}
-        if investment.ticker in stocks_dict:
-            stock_consolidated = stocks_dict[investment.ticker]
-        else:
-            data = self.market_data.ticker_intraday_date(investment.ticker)
-            stock_consolidated = StockConsolidated(ticker=investment.ticker)
-            if data.name.startswith('FII '):
-                print(f'ADDING TO REITS: {investment.ticker}')
-                portfolio.reits.append(stock_consolidated)
-            else:
-                print(f'ADDING TO STOCKS: {investment.ticker}')
-                portfolio.stocks.append(stock_consolidated)
-        stock_consolidated.initial_date = min(stock_consolidated.initial_date, investment.date)
-        stock_consolidated.add_investment(investment)
-
-        self.consolidate_stock_history(stock_consolidated.history, investment)
-
     def calculate_portfolio_performance(self, subject):
         assert subject
         portfolio = self.portfolio_repo.find(subject)
 
-        stock_performance = self._calculate_stocks_performance(portfolio.stocks)
-        reit_performance = self._calculate_stocks_performance(portfolio.reits)
+        stocks = []
+        reits = []
+        for stock in portfolio.stocks:
+            data = self.market_data.ticker_intraday_date(stock.ticker)
+            if data.name.startswith('FII '):
+                reits.append(stock)
+            else:
+                stocks.append(stock)
+        stock_performance = self._calculate_stocks_performance(stocks)
+        reit_performance = self._calculate_stocks_performance(reits)
+
+        portfolio.stocks = stocks
+        portfolio.reits = reits
 
         portfolio.stocks, portfolio.stock_gross_amount, portfolio.stock_prev_gross_amount = stock_performance
         portfolio.reits, portfolio.reit_gross_amount, portfolio.reit_prev_gross_amount = reit_performance
@@ -187,86 +157,3 @@ class PerformanceCore:
                 print(f"REMOVING {stock.ticker}")
                 zeroed_stocks.append(stock)
         return [stock for stock in stocks if stock not in zeroed_stocks], gross_amount, prev_gross_amount
-
-    def consolidate_portfolio_l(self, subject, investments: List[StockInvestment]):
-        portfolio = self.portfolio_repo.find(subject) or Portfolio(subject=subject)
-
-        for investment in investments:
-            print(f'Processing investment: {investment}')
-            # if investment.ticker != 'BIDI11':
-            #     continue
-            portfolio.initial_date = min(investment.date, portfolio.initial_date)
-
-            if investment.type == InvestmentsType.STOCK:
-                value = investment.amount * investment.price * (
-                    -1 if investment.operation == OperationType.SELL else 1)
-                print(f'VALUE: {value}')
-                portfolio.invested_amount = portfolio.invested_amount + value
-                self.consolidate_portfolio_history(portfolio, investment)
-                self.consolidate_stock(portfolio, investment)
-
-        self.portfolio_repo.save(portfolio)
-
-    def consolidate_portfolio(self, subject, investment: StockInvestment):
-        """
-            Method responsible to include a new investment or investment edit/delete and consolidate the information
-             for faster consumption on a later time
-        """
-        portfolio = self.portfolio_repo.find(subject) or Portfolio(subject=subject)
-        portfolio.initial_date = min(investment.date, portfolio.initial_date)
-
-        if investment.type == InvestmentsType.STOCK:
-            value = investment.amount * investment.price * (-1 if investment.operation == OperationType.SELL else 1)
-
-            print(f'VALUE: {value}')
-            portfolio.invested_amount = portfolio.invested_amount + value
-            self.consolidate_portfolio_history(portfolio, investment)
-            self.consolidate_stock(portfolio, investment)
-
-        self.portfolio_repo.save(portfolio)
-
-    def consolidate_stock_history(self, history: List[StockPosition], investment: StockInvestment):
-        month_datetime = DatetimeUtils.month_first_day_datetime(investment.date)
-        month_timestamp = int(month_datetime.timestamp())
-
-        history_dict = {int(h.date.timestamp()): h for h in history}
-        amount = investment.amount if investment.operation == OperationType.BUY else -1 * investment.amount
-
-        if month_timestamp not in history_dict:
-            candle = self.market_data.ticker_month_data(investment.ticker, investment.date)
-            prev_month_timestamp = int(
-                DatetimeUtils.month_first_day_datetime(investment.date - relativedelta(months=1)).timestamp())
-
-            position = StockPosition(date=month_datetime, open_price=candle.open, close_price=candle.close)
-            history.append(position)
-            history_dict[month_timestamp] = position
-
-            self._fix_stock_history_gap(history, history_dict, investment.ticker,
-                                        DatetimeUtils.month_first_day_datetime(datetime.now()))
-
-            if prev_month_timestamp in history_dict:
-                position.amount = position.amount + history_dict[prev_month_timestamp].amount
-
-        for timestamp in list(filter(lambda d: d >= month_timestamp, history_dict.keys())):
-            print(f"Updating history in timestamp: {timestamp}")
-            history_dict[timestamp].amount = history_dict[timestamp].amount + amount
-
-    def _fix_portfolio_history_gap(self, history, history_dict, date_to=None):
-        timestamps = list(history_dict.keys())
-        if len(timestamps) > 1:
-            timestamps.sort()
-            prev = datetime.fromtimestamp(timestamps[0])
-            proc = prev + relativedelta(months=1)
-            last = date_to or datetime.fromtimestamp(timestamps[-1])
-
-            while proc <= last:
-                print(f"PROC: {proc}")
-                proc_timestamp = int(proc.timestamp())
-                if proc_timestamp not in timestamps:
-                    print(f'fix gap: {proc}')
-                    position = PortfolioPosition(date=proc,
-                                                 total_invested=history_dict[int(prev.timestamp())].total_invested)
-                    history.append(position)
-                    history_dict[proc_timestamp] = position
-                prev = proc
-                proc = proc + relativedelta(months=1)
