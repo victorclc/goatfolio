43f49f0676c35a9042145561814698150d330266
Victor Corte <victorcortelc@gmail.com>
2021-03-18 23:47:27 -0300

    cleanir rentability card

--- lib/pages/summary/widget/month_summary_card.dart
+++ lib/pages/summary/widget/month_summary_card.dart
@@ -62,84 +62,70 @@ class _MonthSummaryCardState extends State<MonthSummaryCard> {
                   SizedBox(
                     height: 8,
                   ),
-                  Row(
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          'Variação',
-                          style: textTheme.tabLabelTextStyle
-                              .copyWith(fontSize: 16),
-                        ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Variação no mês: ',
+                            style: textTheme.tabLabelTextStyle
+                                .copyWith(fontSize: 16),
+                          ),
+                          Text(
+                            moneyFormatter.format(
+                                widget.performance.grossAmount -
+                                    widget
+                                        .performance
+                                        .history[widget.performance
+                                                .history.length -
+                                            2]
+                                        .grossAmount),
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyText2
+                                .copyWith(
+                                    color: widget.performance
+                                                    .grossAmount -
+                                                widget
+                                                    .performance
+                                                    .history[widget
+                                                            .performance
+                                                            .history
+                                                            .length -
+                                                        2]
+                                                    .grossAmount >=
+                                            0
+                                        ? Colors.green
+                                        : Colors.red,
+                                    fontSize: 16),
+                          ),
+                        ],
                       ),
-                      Expanded(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  ' mês: ',
-                                  style: textTheme.tabLabelTextStyle
-                                      .copyWith(fontSize: 16),
-                                ),
-                                Text(
-                                  moneyFormatter.format(
-                                      widget.performance.grossAmount -
-                                          widget
-                                              .performance
-                                              .history[widget.performance
-                                                      .history.length -
-                                                  2]
-                                              .grossAmount),
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .bodyText2
-                                      .copyWith(
-                                          color: widget.performance
-                                                          .grossAmount -
-                                                      widget
-                                                          .performance
-                                                          .history[widget
-                                                                  .performance
-                                                                  .history
-                                                                  .length -
-                                                              2]
-                                                          .grossAmount >=
-                                                  0
-                                              ? Colors.green
-                                              : Colors.red,
-                                          fontSize: 16),
-                                ),
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  ' dia: ',
-                                  style: textTheme.tabLabelTextStyle
-                                      .copyWith(fontSize: 16),
-                                ),
-                                Text(
-                                  moneyFormatter
-                                      .format(widget.performance.dayVariation),
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .bodyText2
-                                      .copyWith(
-                                          color:
-                                              widget.performance.dayVariation >=
-                                                      0
-                                                  ? Colors.green
-                                                  : Colors.red,
-                                          fontSize: 16),
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            'Variação no dia: ',
+                            style: textTheme.tabLabelTextStyle
+                                .copyWith(fontSize: 16),
+                          ),
+                          Text(
+                            moneyFormatter
+                                .format(widget.performance.dayVariation),
+                            style: Theme.of(context)
+                                .textTheme
+                                .bodyText2
+                                .copyWith(
+                                    color:
+                                        widget.performance.dayVariation >=
+                                                0
+                                            ? Colors.green
+                                            : Colors.red,
+                                    fontSize: 16),
+                          ),
+                        ],
                       ),
                     ],
                   ),
