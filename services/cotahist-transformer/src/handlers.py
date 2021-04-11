3b089906b6721d6109a27b6bf7049852f6ae2d8c
Victor Corte <victorcortelc@gmail.com>
2021-04-11 20:07:09 -0300

    mora logs

--- services/cotahist-transformer/src/handlers.py
+++ services/cotahist-transformer/src/handlers.py
@@ -6,12 +6,12 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s | %(funcName)s %(lev
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-core = CotaHistTransformerCore()
-
 
 def transform_cota_hist_handler(event, context):
     logger.info(f'EVENT: {event}')
+    core = CotaHistTransformerCore()
     for record in event['Records']:
+        logger.info(f'Processing record: {record}')
         bucket = record['s3']['bucket']['name']
         file_path = record['s3']['object']['key']
         core.transform_cota_hist(bucket, file_path)
