979544384b2ba8433793622d5c4d624562b2bdef
Victor Corte <victorcortelc@gmail.com>
2021-04-18 16:39:46 -0300

    fields inclusion

--- lib/services/performance/model/stock_position.dart
+++ lib/services/performance/model/stock_position.dart
@@ -3,12 +3,14 @@ class StockPosition {
   double openPrice;
   double closePrice;
   double amount;
-  double investedAmount;
+  double investedValue;
+  double soldValue;
 
   StockPosition.fromJson(Map<String, dynamic> json)
       : date = DateTime.fromMillisecondsSinceEpoch(json['date'] * 1000, isUtc: true),
         openPrice = json['open_price'],
         closePrice = json['close_price'],
         amount = json['amount'],
-        investedAmount = json['invested_amount'];
+        soldValue = json['sold_value'],
+        investedValue = json['invested_value'];
 }
