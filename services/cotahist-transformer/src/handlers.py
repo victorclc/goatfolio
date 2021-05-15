5afa3077022f56df9b96092416e09353620d4e27
Victor Corte <victorcortelc@gmail.com>
2021-05-15 15:05:44 -0300

    shitnotifier on cotahist transformer

--- services/cotahist-transformer/src/handlers.py
+++ services/cotahist-transformer/src/handlers.py
@@ -1,6 +1,9 @@
 import logging
+import traceback
 
 from core import CotaHistTransformerCore
+from goatcommons.shit.client import ShitNotifierClient
+from goatcommons.shit.models import NotifyLevel
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s | %(funcName)s %(levelname)-s: %(message)s')
 logger = logging.getLogger()
@@ -8,10 +11,14 @@ logger.setLevel(logging.INFO)
 
 
 def transform_cota_hist_handler(event, context):
-    logger.info(f'EVENT: {event}')
-    core = CotaHistTransformerCore()
-    for record in event['Records']:
-        logger.info(f'Processing record: {record}')
-        bucket = record['s3']['bucket']['name']
-        file_path = record['s3']['object']['key']
-        core.transform_cota_hist(bucket, file_path)
+    try:
+        logger.info(f'EVENT: {event}')
+        core = CotaHistTransformerCore()
+        for record in event['Records']:
+            logger.info(f'Processing record: {record}')
+            bucket = record['s3']['bucket']['name']
+            file_path = record['s3']['object']['key']
+            core.transform_cota_hist(bucket, file_path)
+    except Exception:
+        ShitNotifierClient().send(NotifyLevel.ERROR, 'COTAHIST-TRANSFORMER',
+                                  f'TRANSFORM COTA HIST FAILED {traceback.format_exc()}')
