a4d534034f7dd692bac5407463afdfb193aa36da
Victor Corte <victorcortelc@gmail.com>
2021-05-15 00:09:23 -0300

    cleanup shit client

--- libs/goatcommons/shit/client.py
+++ libs/goatcommons/shit/client.py
@@ -1,10 +1,9 @@
 from dataclasses import asdict
-from goatcommons.shit.models import NotifyRequest, NotifyLevel
-from goatcommons.utils import JsonUtils
 
 import boto3
 
-boto3.setup_default_session(profile_name='dev')
+from goatcommons.shit.models import NotifyRequest
+from goatcommons.utils import JsonUtils
 
 
 class ShitNotifierClient:
@@ -12,6 +11,5 @@ class ShitNotifierClient:
         self._queue = boto3.resource('sqs').get_queue_by_name(QueueName='ShitToNotify')
 
     def send(self, level, service, message):
-        request = NotifyRequest(level, service, message);
+        request = NotifyRequest(level, service, message)
         self._queue.send_message(MessageBody=JsonUtils.dump(asdict(request)))
-
