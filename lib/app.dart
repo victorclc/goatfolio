f5aa576782488f93d809494dd4ceb79857966b02
Victor Corte <victorcortelc@gmail.com>
2021-07-16 10:29:35 -0300

    adjusted mains

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,53 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:goatfolio/pages/login/screen/login.dart';
+import 'package:goatfolio/pages/navigation/screen/navigation.dart';
+import 'package:goatfolio/services/authentication/service/cognito.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'common/theme/theme_changer.dart';
 import 'flavors.dart';
-import 'pages/my_home_page.dart';
 
-class App extends StatelessWidget {
+
+class GoatfolioApp extends StatelessWidget {
+  final bool hasValidSession;
+  final UserService userService;
+  final SharedPreferences prefs;
+
+  const GoatfolioApp(
+      {Key key, this.hasValidSession, this.userService, this.prefs})
+      : super(key: key);
+
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: F.title,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+  Widget build(context) {
+    return ChangeNotifierProvider<ThemeChanger>(
+      create: (_) => ThemeChanger(prefs),
+      child: MaterialApp(
+        title: F.title,
+        theme: ThemeData(),
+        darkTheme: ThemeData.light(),
+        localizationsDelegates: [
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: [const Locale('pt', 'BR')],
+        builder: (context, child) {
+          return CupertinoTheme(
+            data: Provider.of<ThemeChanger>(context).themeData,
+            child: Material(child: child),
+          );
+        },
+        home: Scaffold(
+          body: hasValidSession
+              ? buildNavigationPage(userService)
+              : LoginPage(
+            userService: userService,
+            onLoggedOn: goToNavigationPage,
+          ),
+        ),
       ),
-      home: MyHomePage(),
     );
   }
 }
--- lib/main-dev.dart
+++ lib/main-dev.dart
@@ -1,8 +1,37 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'app.dart';
-import 'flavors.dart';
+import 'package:goatfolio/app.dart';
+import 'package:goatfolio/common/config/app_config.dart';
+import 'package:goatfolio/flavors.dart';
+import 'package:goatfolio/services/authentication/service/cognito.dart';
+import 'package:goatfolio/services/notification/firebase/firebase.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-void main() {
+
+final cognitoClientId = '4eq433usu00k6m0as28srbsber';
+final cognitoUserPoolId = 'us-east-2_tZFglntHx';
+final cognitoIdentityPoolId =
+    'arn:aws:cognito-idp:us-east-2:831967415635:userpool/us-east-2_tZFglntHx';
+
+void main() async {
   F.appFlavor = Flavor.DEV;
-  runApp(App());
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeFirebaseNotifications();
+  final userService =
+  UserService(cognitoUserPoolId, cognitoClientId, cognitoIdentityPoolId);
+
+  final configuredApp = new AppConfig(
+    cognitoClientId: cognitoClientId,
+    cognitoUserPoolId: cognitoUserPoolId,
+    cognitoIdentityPoolId: cognitoIdentityPoolId,
+    child: new GoatfolioApp(
+      hasValidSession: await userService.init(),
+      userService: userService,
+      prefs: await SharedPreferences.getInstance(),
+    ),
+  );
+
+  runApp(configuredApp);
 }
+
+
