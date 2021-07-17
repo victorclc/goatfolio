f1bbfc603715e8b2fa9532c9594284d699157fe7
Victor Corte <victorcortelc@gmail.com>
2021-07-17 13:56:28 -0300

    configured cognitoPool in flavors

--- lib/flavors.dart
+++ lib/flavors.dart
@@ -8,13 +8,41 @@ class F {
 
   static String get title {
     switch (appFlavor) {
-      case Flavor.DEV:
-        return 'Goatfolio Dev';
       case Flavor.PROD:
         return 'Goatfolio';
+      case Flavor.DEV:
       default:
-        return 'title';
+        return 'Goatfolio Dev';
     }
   }
 
+  static String get cognitoClientId {
+    switch (appFlavor) {
+      case Flavor.PROD:
+        return '4eq433usu00k6m0as28srbsber';
+      case Flavor.DEV:
+      default:
+        return '4eq433usu00k6m0as28srbsber';
+    }
+  }
+
+  static String get cognitoUserPoolId {
+    switch (appFlavor) {
+      case Flavor.PROD:
+        return 'us-east-2_tZFglntHx';
+      case Flavor.DEV:
+      default:
+        return 'us-east-2_tZFglntHx';
+    }
+  }
+
+  static String get cognitoIdentityPoolId {
+    switch (appFlavor) {
+      case Flavor.PROD:
+        return 'arn:aws:cognito-idp:us-east-2:831967415635:userpool/us-east-2_tZFglntHx';
+      case Flavor.DEV:
+      default:
+        return 'arn:aws:cognito-idp:us-east-2:831967415635:userpool/us-east-2_tZFglntHx';
+    }
+  }
 }
--- lib/main-dev.dart
+++ lib/main-dev.dart
@@ -1,37 +1,24 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:goatfolio/app.dart';
-import 'package:goatfolio/common/config/app_config.dart';
 import 'package:goatfolio/flavors.dart';
 import 'package:goatfolio/services/authentication/service/cognito.dart';
 import 'package:goatfolio/services/notification/firebase/firebase.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-
-final cognitoClientId = '4eq433usu00k6m0as28srbsber';
-final cognitoUserPoolId = 'us-east-2_tZFglntHx';
-final cognitoIdentityPoolId =
-    'arn:aws:cognito-idp:us-east-2:831967415635:userpool/us-east-2_tZFglntHx';
-
 void main() async {
   F.appFlavor = Flavor.DEV;
   WidgetsFlutterBinding.ensureInitialized();
   await initializeFirebaseNotifications();
-  final userService =
-  UserService(cognitoUserPoolId, cognitoClientId, cognitoIdentityPoolId);
-
-  final configuredApp = new AppConfig(
-    cognitoClientId: cognitoClientId,
-    cognitoUserPoolId: cognitoUserPoolId,
-    cognitoIdentityPoolId: cognitoIdentityPoolId,
-    child: new GoatfolioApp(
-      hasValidSession: await userService.init(),
-      userService: userService,
-      prefs: await SharedPreferences.getInstance(),
-    ),
+  final userService = UserService(
+    F.cognitoUserPoolId,
+    F.cognitoClientId,
+    F.cognitoIdentityPoolId,
   );
-
-  runApp(configuredApp);
+  final app = new GoatfolioApp(
+    hasValidSession: await userService.init(),
+    userService: userService,
+    prefs: await SharedPreferences.getInstance(),
+  );
+  runApp(app);
 }
-
-
