70cc5f771aedc3a72ddfff52a2a6bf3c4a945fab
Victor Corte <victorcortelc@gmail.com>
2021-08-05 16:36:08 -0300

    env variables and queue name adjustment

--- libs/goatcommons/shit/client.py
+++ libs/goatcommons/shit/client.py
@@ -8,7 +8,7 @@ from goatcommons.utils import JsonUtils
 
 class ShitNotifierClient:
     def __init__(self):
-        self._queue = boto3.resource('sqs').get_queue_by_name(QueueName='ShitToNotify')
+        self._queue = boto3.resource('sqs').get_queue_by_name(QueueName='EventsToNotify')
 
     def send(self, level, service, message):
         request = NotifyRequest(level, service, message)
