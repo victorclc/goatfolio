bff329e12897e3e1d85a08212eaae11dc7bb019d
Victor Corte <victorcortelc@gmail.com>
2021-08-11 11:11:31 -0300

    android notifications dialog

--- lib/common/util/dialog.dart
+++ lib/common/util/dialog.dart
@@ -1,21 +1,35 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class DialogUtils {
   static Future<void> showSuccessDialog(BuildContext context, String message,
       [String title = "Tudo certo!"]) async {
-    return _cupertinoNotifyDialog(context, title, message);
+    if (Platform.isIOS) {
+      return _cupertinoNotifyDialog(context, title, message);
+    } else {
+      return _androidNotifyDialog(context, title, message);
+    }
   }
 
   static Future<void> showErrorDialog(BuildContext context, String message,
       [String title = "Erro"]) async {
-    return _cupertinoNotifyDialog(context, title, message);
+    if (Platform.isIOS) {
+      return _cupertinoNotifyDialog(context, title, message);
+    } else {
+      return _androidNotifyDialog(context, title, message);
+    }
   }
 
   static Future<void> showCustomErrorDialog(
       BuildContext context, Widget content,
       [String title = "Erro"]) async {
-    return _cupertinoCustomNotifyDialog(context, title, content);
+    if (Platform.isIOS) {
+      return _cupertinoCustomNotifyDialog(context, title, content);
+    } else {
+      return _androidCustomNotifyDialog(context, title, content);
+    }
   }
 
   static Future<void> showNoYesDialog(BuildContext context,
@@ -23,16 +37,29 @@ class DialogUtils {
       String message,
       Function onYesPressed,
       Function onNoPressed}) async {
-    return _cupertinoNoYesDialog(context, title: title, message: message,
-        onYesPressed: () async {
-      if (onYesPressed != null) onYesPressed();
-      Navigator.of(context).pop();
-    }, onNoPressed: () async {
-      if (onNoPressed != null) {
-        onNoPressed();
-      }
-      Navigator.pop(context);
-    });
+    if (Platform.isIOS) {
+      return _cupertinoNoYesDialog(context, title: title, message: message,
+          onYesPressed: () async {
+        if (onYesPressed != null) onYesPressed();
+        Navigator.of(context).pop();
+      }, onNoPressed: () async {
+        if (onNoPressed != null) {
+          onNoPressed();
+        }
+        Navigator.pop(context);
+      });
+    } else {
+      return _androidNoYesDialog(context, title: title, message: message,
+          onYesPressed: () async {
+        if (onYesPressed != null) onYesPressed();
+        Navigator.of(context).pop();
+      }, onNoPressed: () async {
+        if (onNoPressed != null) {
+          onNoPressed();
+        }
+        Navigator.pop(context);
+      });
+    }
   }
 
   static Future<void> _cupertinoNoYesDialog(BuildContext context,
@@ -52,6 +79,26 @@ class DialogUtils {
         textStyle: TextStyle(color: Colors.red),
         onPressed: onYesPressed,
         child: Text("Excluir"),
+      ),
+    ]);
+  }
+
+  static Future<void> _androidNoYesDialog(BuildContext context,
+      {String title,
+      String message,
+      Function onYesPressed,
+      Function onNoPressed}) async {
+    await _androidDialog(context, title, message, [
+      SimpleDialogOption(
+        onPressed: onNoPressed,
+        child: Text("Cancelar"),
+      ),
+      SimpleDialogOption(
+        onPressed: onYesPressed,
+        child: Text(
+          "Excluir",
+          style: TextStyle(color: Colors.red),
+        ),
       ),
     ]);
   }
@@ -78,15 +125,23 @@ class DialogUtils {
     ]);
   }
 
-  static Future<void> androidNotifyDialog(
+  static Future<void> _androidNotifyDialog(
       BuildContext context, String title, String message) async {
     await _androidDialog(context, title, message, [
       SimpleDialogOption(
-          child: CupertinoButton(
-        padding: EdgeInsets.zero,
-        child: Text("OK"),
         onPressed: () => Navigator.of(context).pop(),
-      )),
+        child: Text("OK"),
+      ),
+    ]);
+  }
+
+  static Future<void> _androidCustomNotifyDialog(
+      BuildContext context, String title, Widget content) async {
+    await _androidCustomDialog(context, title, content, [
+      SimpleDialogOption(
+        onPressed: () => Navigator.of(context).pop(),
+        child: Text("OK"),
+      ),
     ]);
   }
 
@@ -128,6 +183,20 @@ class DialogUtils {
         return AlertDialog(
           title: Text(title),
           content: Text(message),
+          actions: actions,
+        );
+      },
+    );
+  }
+
+  static Future<void> _androidCustomDialog(BuildContext context, String title,
+      Widget content, List<Widget> actions) async {
+    await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text(title),
+          content: content,
           actions: actions,
         );
       },
