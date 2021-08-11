1d26f686f429dee3f12a80a70abaf82a09293e73
Victor Corte <victorcortelc@gmail.com>
2021-08-11 11:30:42 -0300

    material bottom sheet

--- lib/common/util/modal.dart
+++ lib/common/util/modal.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 
 class ModalUtils {
@@ -17,14 +20,36 @@ class ModalUtils {
   static Future<T> showDragableModalBottomSheet<T>(
       BuildContext context, Widget child,
       {bool expandable = true, bool isDismissible = false}) async {
-    return await showCupertinoModalBottomSheet(
-        enableDrag: true,
+    if (Platform.isIOS) {
+      return await showCupertinoModalBottomSheet(
+          enableDrag: true,
+          isDismissible: isDismissible,
+          useRootNavigator: true,
+          expand: expandable,
+          context: context,
+          builder: (context) {
+            return child;
+          });
+    } else {
+      return _showAndroidDragableModalBottomSheet(
+        context,
+        child,
+        expandable: expandable,
         isDismissible: isDismissible,
-        useRootNavigator: true,
-        expand: expandable,
-        context: context,
-        builder: (context) {
-          return child;
-        });
+      );
+    }
+  }
+
+  static Future<T> _showAndroidDragableModalBottomSheet<T>(
+      BuildContext context, Widget child,
+      {bool expandable = true, bool isDismissible = false}) async {
+    return await showMaterialModalBottomSheet(
+      context: context,
+      builder: (_) => child,
+      expand: expandable,
+      enableDrag: true,
+      useRootNavigator: true,
+      isDismissible: isDismissible,
+    );
   }
 }
