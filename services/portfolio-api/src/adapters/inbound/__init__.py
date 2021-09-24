02c333f77191d556d8c7c0b14c8269df2fd772ec
Victor Corte <victorcortelc@gmail.com>
2021-09-24 16:57:02 -0300

    fix imports

--- services/portfolio-api/src/adapters/inbound/__init__.py
+++ services/portfolio-api/src/adapters/inbound/__init__.py
@@ -1,5 +1,4 @@
 from adapters.outbound.dynamo_investment_repository import DynamoInvestmentRepository
-from adapters.outbound.dynamo_stock_history_repository import MarketData
 from adapters.outbound.dynamo_portfolio_repository import DynamoPortfolioRepository
 from domain.investment_core import InvestmentCore
 from domain.performance_core import PerformanceCore
@@ -7,6 +6,4 @@ from domain.portfolio_core import PortfolioCore
 
 investment_core = InvestmentCore(repo=DynamoInvestmentRepository())
 portfolio_core = PortfolioCore(repo=DynamoPortfolioRepository())
-performance_core = PerformanceCore(
-    repo=DynamoPortfolioRepository(), market_data=MarketData()
-)
+performance_core = PerformanceCore(DynamoPortfolioRepository())
