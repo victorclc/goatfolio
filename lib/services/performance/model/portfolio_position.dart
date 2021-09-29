a2075ff1474aa9478613c398979b5ae997a8463d
Victor Corte <victorcortelc@gmail.com>
2021-09-29 09:54:20 -0300

    history parse

--- lib/services/performance/model/portfolio_position.dart
+++ lib/services/performance/model/portfolio_position.dart
@@ -1,10 +1,12 @@
+import 'package:intl/intl.dart';
+
 class PortfolioPosition {
   DateTime date;
   double investedValue;
   double grossValue;
 
   PortfolioPosition.fromJson(Map<String, dynamic> json)
-      : date = DateTime.fromMillisecondsSinceEpoch(json['date'] * 1000, isUtc: true),
+      : date = DateTime.parse(json['date']),
         investedValue = json['invested_value'],
         grossValue = json['gross_value'];
 }
--- lib/services/performance/model/stock_consolidated_position.dart
+++ lib/services/performance/model/stock_consolidated_position.dart
@@ -5,8 +5,7 @@ class StockConsolidatedPosition {
   double variationPerc;
 
   StockConsolidatedPosition.fromJson(Map<String, dynamic> json)
-      : date = DateTime.fromMillisecondsSinceEpoch(json['date'] * 1000,
-            isUtc: true),
+      : date = DateTime.parse(json['date']),
         grossValue = json['gross_value'],
         investedValue = json['invested_value'],
         variationPerc = json['variation_perc'];
