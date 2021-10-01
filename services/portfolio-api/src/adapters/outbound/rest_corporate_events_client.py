f0e3b89eb9d79303aff5a46d8615c6459bd9c605
Victor Corte <victorcortelc@gmail.com>
2021-10-01 11:10:53 -0300

    environment variables and api-key from parameter store

--- services/portfolio-api/src/adapters/outbound/rest_corporate_events_client.py
+++ services/portfolio-api/src/adapters/outbound/rest_corporate_events_client.py
@@ -1,19 +1,22 @@
 import datetime
+import os
 
 import requests
 
 from domain.models.ticker_transformation import TickerTransformation
+from goatcommons.configuration.system_manager import ConfigurationClient
 
 
 class RESTCorporateEventsClient:
-    BASE_URL = "https://dev.goatfolio.com.br/corporate-events"
+    BASE_URL = os.getenv("CORPORATE_EVENTS_URL")
 
     def __init__(self):
-        # config = ConfigurationClient()
-        # key = config.get_secret("corporate-events-key")
-        self.api_key = "MV9Bmm4WA892LuRVQmb1c6p0kRQlUQqb9KXYUNsL"
+        config = ConfigurationClient()
+        self.api_key = config.get_secret("corporate-events-api-key")
 
-    def get_ticker_transformation(self, ticker: str, date: datetime.date) -> TickerTransformation:
+    def get_ticker_transformation(
+        self, ticker: str, date: datetime.date
+    ) -> TickerTransformation:
         url = f"{self.BASE_URL}/transformations?ticker={ticker}&dateFrom={date.strftime('%Y%m%d')}"
         response = requests.get(url, headers={"x-api-key": self.api_key})
 
