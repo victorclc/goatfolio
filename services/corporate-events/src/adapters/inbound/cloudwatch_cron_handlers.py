6c582a714c8824fc3035cf243ca531dcb9fad652
Victor Corte <victorcortelc@gmail.com>
2021-10-01 09:24:38 -0300

    creating objects in __init__.py and check for appicable corporate events

--- services/corporate-events/src/adapters/inbound/cloudwatch_cron_handlers.py
+++ services/corporate-events/src/adapters/inbound/cloudwatch_cron_handlers.py
@@ -3,20 +3,11 @@ import logging
 
 from dateutil.relativedelta import relativedelta
 
-from adapters.outbound.dynamo_corporate_events_repository import (
-    DynamoCorporateEventsRepository,
-)
-from adapters.outbound.dynamo_investment_repository import DynamoInvestmentRepository
-from adapters.outbound.dynamo_ticker_info_client import DynamoTickerInfoClient
-from adapters.outbound.s3_coporate_events_file_storage import (
-    S3CorporateEventsFileStorage,
-)
-from domain.core.corporate_events_core import CorporateEventsCore
-from domain.core.corporate_events_crawler import B3CorporateEventsCrawler
+import domain.core.strategy as strategies
+from adapters.inbound import corporate_events_crawler, corporate_events_core
 from domain.enums.event_type import EventType
 from event_notifier.decorators import notify_exception
 from event_notifier.models import NotifyLevel
-import domain.core.strategy as strategies
 
 logging.basicConfig(
     level=logging.INFO, format="%(asctime)s | %(funcName)s %(levelname)-s: %(message)s"
@@ -24,26 +15,18 @@ logging.basicConfig(
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-events_repo = DynamoCorporateEventsRepository()
-investment_repo = DynamoInvestmentRepository()
-ticker_client = DynamoTickerInfoClient()
-file_storage = S3CorporateEventsFileStorage()
-
-core = CorporateEventsCore(events_repo, ticker_client, investment_repo)
-
 
 @notify_exception(Exception, NotifyLevel.CRITICAL)
 def craw_today_corporate_events_handler(event, context):
     today = datetime.datetime.now().date()
-    crawler = B3CorporateEventsCrawler(file_storage)
 
-    crawler.craw_corporate_events_from_date(today)
+    corporate_events_crawler.craw_corporate_events_from_date(today)
 
 
 @notify_exception(Exception, NotifyLevel.CRITICAL)
 def handle_yesterday_split_events_handler(event, context):
     yesterday = datetime.datetime.now().date() - relativedelta(days=1)
-    core.handle_corporate_events(
+    corporate_events_core.handle_corporate_events(
         EventType.SPLIT, yesterday, strategies.handle_split_event_strategy
     )
 
@@ -51,7 +34,7 @@ def handle_yesterday_split_events_handler(event, context):
 @notify_exception(Exception, NotifyLevel.CRITICAL)
 def handle_today_group_events_handler(event, context):
     yesterday = datetime.datetime.now().date() - relativedelta(days=1)
-    core.handle_corporate_events(
+    corporate_events_core.handle_corporate_events(
         EventType.GROUP, yesterday, strategies.handle_group_event_strategy
     )
 
@@ -59,7 +42,7 @@ def handle_today_group_events_handler(event, context):
 @notify_exception(Exception, NotifyLevel.CRITICAL)
 def handle_today_incorporation_events_handler(event, context):
     yesterday = datetime.datetime.now().date() - relativedelta(days=1)
-    core.handle_corporate_events(
+    corporate_events_core.handle_corporate_events(
         EventType.INCORPORATION,
         yesterday,
         strategies.handle_incorporation_event_strategy,
@@ -68,7 +51,7 @@ def handle_today_incorporation_events_handler(event, context):
 
 def main():
     date_from = datetime.datetime.now().date() - relativedelta(months=18)
-    print(core.transformations_in_ticker("ITSA4", date_from))
+    print(corporate_events_core.transformations_in_ticker("AESB3", date_from))
 
 
 if __name__ == "__main__":
--- services/corporate-events/src/domain/core/corporate_events_core.py
+++ services/corporate-events/src/domain/core/corporate_events_core.py
@@ -2,11 +2,13 @@ import datetime
 import logging
 from decimal import Decimal
 from itertools import groupby
-from typing import Callable, List
+from typing import Callable, List, Dict
 
 from domain.enums.event_type import EventType
+from domain.enums.operation_type import OperationType
 from domain.models.earnings_in_assets_event import EarningsInAssetCorporateEvent
 from domain.models.stock_investment import StockInvestment
+from domain.models.ticker_transformation import TickerTransformation
 from domain.ports.outbound.corporate_events_repository import CorporateEventsRepository
 from domain.ports.outbound.investment_repository import InvestmentRepository
 from domain.ports.outbound.ticker_info_client import TickerInfoClient
@@ -61,14 +63,52 @@ class CorporateEventsCore:
                 if new_investments:
                     self.investments.batch_save(new_investments)
 
+    def check_for_applicable_corporate_events(
+        self, subject, investments, strategies: Dict[EventType, Callable]
+    ):
+        investments = filter(
+            lambda i: i.operation in [OperationType.BUY, OperationType.SELL]
+            and not i.alias_ticker,
+            investments,
+        )
+        investments_map = groupby(
+            sorted(investments, key=lambda i: i.ticker), key=lambda i: i.ticker
+        )
+
+        for ticker, investments in investments_map:
+            investments = list(investments)
+            oldest = min([i.date for i in investments])
+            isin_code = self.ticker.get_isin_code_from_ticker(ticker)
+            events = self.events.find_by_isin_from_date(isin_code, oldest)
+
+            if not events:
+                continue
+
+            all_ticker_investments = self.investments.find_by_subject_and_ticker(
+                subject, ticker
+            )
+            for event in events:
+                logger.info(f"handling event {event} for {subject}")
+                affected_investments = list(
+                    filter(
+                        lambda i, with_date=event.with_date: i.date <= with_date,
+                        all_ticker_investments,
+                    )
+                )
+                new_investments = strategies[event.type](
+                    subject, ticker, event, list(affected_investments), self.ticker
+                )
+                if new_investments:
+                    self.investments.batch_save(new_investments)
+
     def transformations_in_ticker(self, current_ticker: str, date_from: datetime.date):
         ticker = current_ticker
         isin = self.ticker.get_isin_code_from_ticker(ticker)
+        events_list = self.events.find_by_isin_from_date(isin, date_from)
+
         tmp = self.events.find_by_type_and_emitted_asset(
             EventType.INCORPORATION, isin, date_from
         )
-
-        events_list = self.events.find_by_isin_from_date(isin, date_from)
         if tmp:
             previous_isin = tmp[0].isin_code
             ticker = self.ticker.get_ticker_from_isin_code(previous_isin)
@@ -78,4 +118,4 @@ class CorporateEventsCore:
         for event in events_list:
             factor *= event.factor
 
-        return ticker, factor
+        return TickerTransformation(ticker, factor)
