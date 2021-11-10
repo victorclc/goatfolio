f709382d31b50f4f1c089e7e35f1b6cd016ebc77
Victor Corte <victorcortelc@gmail.com>
2021-11-10 18:03:13 -0300

    cognito prod data

--- mobile/lib/flavors.dart
+++ mobile/lib/flavors.dart
@@ -19,7 +19,7 @@ class F {
   static String get cognitoClientId {
     switch (appFlavor) {
       case Flavor.PROD:
-        return '7ovg7avdp7r6s02kpc6oc6hl04';
+        return '3gcs81tod4nb9hhfkipj4plsvp';
       case Flavor.DEV:
       default:
         return '30mcm6342c56fj7da8cqmns2f0';
@@ -29,7 +29,7 @@ class F {
   static String get cognitoUserPoolId {
     switch (appFlavor) {
       case Flavor.PROD:
-        return 'sa-east-1_eGtDKQt4X';
+        return 'sa-east-1_kPXadCr4Z';
       case Flavor.DEV:
       default:
         return 'sa-east-1_PhDIztXK0';
@@ -39,7 +39,7 @@ class F {
   static String get cognitoIdentityPoolId {
     switch (appFlavor) {
       case Flavor.PROD:
-        return 'arn:aws:cognito-idp:sa-east-1:810300526230:userpool/sa-east-1_eGtDKQt4X';
+        return 'arn:aws:cognito-idp:sa-east-1:810300526230:userpool/sa-east-1_kPXadCr4Z';
       case Flavor.DEV:
       default:
         return 'arn:aws:cognito-idp:sa-east-1:138414734174:userpool/sa-east-1_PhDIztXK0';
