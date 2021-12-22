b2fc4db25a38083b71b112713994a77d8fb9982f
Victor Corte <victorcortelc@gmail.com>
2021-12-22 00:07:47 -0300

    validating ticker on stock consolidation

--- backend/portfolio-api/src/domain/investments/investment_consolidation_core.py
+++ backend/portfolio-api/src/domain/investments/investment_consolidation_core.py
@@ -2,6 +2,7 @@ import logging
 from itertools import groupby
 from typing import List, Dict, Set, Optional
 
+from application.ports.ticker_info_client import TickerInfoClient
 from domain.common.investments import InvestmentType, Investment
 from domain.common.portfolio import (
     Portfolio,
@@ -22,15 +23,28 @@ class InvestmentConsolidationCore:
     def __init__(
         self,
         repo: PortfolioRepository,
+        ticker_client: TickerInfoClient,
         strategies: Dict[InvestmentType, InvestmentsConsolidationStrategy],
     ):
         self.repo = repo
         self.strategies = strategies
+        self.ticker_client = ticker_client
 
     def consolidate_investments(
         self, subject: str, new: Optional[Investment], old: Optional[Investment]
     ):
         portfolio = self.get_portfolio(subject)
+        if new:
+            if not self.ticker_client.is_ticker_valid(new.ticker):
+                logger.warning(f"NEW TICKER IS NOT VALID: {new.ticker}")
+                new = None
+        if old:
+            if not self.ticker_client.is_ticker_valid(old.ticker):
+                logger.warning(f"OLD TICKER IS NOT VALID: {old.ticker}")
+                old = None
+
+        if not new and not old:
+            return
 
         _type = self.get_type_if_investment(new, old)
         inv_consolidated = self.strategies[_type].consolidate(subject, new, old)
