bf48f9300a332c671877ee3c9fa005375899719f
Victor Corte <victorcortelc@gmail.com>
2022-01-15 09:57:21 -0300

    getting manual corporate events from get corporate events api

--- backend/corporate-events/src/adapters/inbound/private_api_gateway_handlers.py
+++ backend/corporate-events/src/adapters/inbound/private_api_gateway_handlers.py
@@ -6,6 +6,7 @@ from http import HTTPStatus
 import goatcommons.utils.aws as awsutils
 import goatcommons.utils.json as jsonutils
 from adapters.inbound import ticker_client, events_repo
+from adapters.outbound.dynamo_manual_corporate_events_repository import DynamoManualCorporateEventsRepository
 from core.get_corporate_events import get_corporate_events
 from core.ticker_transformations import transformations_in_ticker
 
@@ -31,12 +32,14 @@ def get_ticker_transformations_handler(event, context):
 
 
 def get_corporate_events_handler(event, context):
+    subject = awsutils.get_query_param(event, "subject")
     ticker = awsutils.get_query_param(event, "ticker")
     date_from = datetime.datetime.strptime(
         awsutils.get_query_param(event, "dateFrom"), "%Y%m%d"
     ).date()
+    manual_repo = DynamoManualCorporateEventsRepository()
 
-    events = get_corporate_events(ticker, date_from, ticker_client, events_repo)
+    events = get_corporate_events(ticker, date_from, ticker_client, events_repo, manual_repo, subject)
 
     return {
         "statusCode": HTTPStatus.OK,
