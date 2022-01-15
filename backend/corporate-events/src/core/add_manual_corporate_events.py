9882921831fe02d66a8131f25875eefbdc55237f
Victor Corte <victorcortelc@gmail.com>
2022-01-15 09:07:41 -0300

    added logs, fixed handler name, and dumping json on body resposne

--- backend/corporate-events/src/core/add_manual_corporate_events.py
+++ backend/corporate-events/src/core/add_manual_corporate_events.py
@@ -1,12 +1,16 @@
 from datetime import datetime
 from typing import Protocol
 
+from aws_lambda_powertools import Logger
+
 from application.enums.event_type import EventType
 from application.exceptions.validation_errors import InvalidEmittedTickerError, InvalidLastDatePriorError, \
     InvalidGroupingFactorError
 from application.models.earnings_in_assets_event import ManualEarningsInAssetCorporateEvents
 from application.models.manual_event import BonificacaoEvent, IncorporationEvent, GroupEvent, SplitEvent
 from application.ports.ticker_info_client import TickerInfoClient
+
+logger = Logger()
 
 
 class ManualEarningInAssetsRepository(Protocol):
@@ -16,11 +20,13 @@ class ManualEarningInAssetsRepository(Protocol):
 
 def _validate_emitted_ticker(emitted_ticker: str, ticker_client: TickerInfoClient):
     if not ticker_client.is_ticker_valid(emitted_ticker):
+        logger.info(f"Unknown emitted ticker: {emitted_ticker}")
         raise InvalidEmittedTickerError("Código do ativo emitido inválido.")
 
 
 def _validate_last_date_prior(last_date_prior: datetime.date):
     if last_date_prior >= datetime.now().date():
+        logger.info(f"Invalid last date prior: {last_date_prior}")
         raise InvalidLastDatePriorError("A data do evento não pode ser maior ou igual que a data atual.")
 
 
@@ -44,11 +50,13 @@ def add_incorporation_corporate_event(subject: str, incorporation: Incorporation
                                                  grouping_factor=incorporation.grouping_factor,
                                                  emitted_ticker=incorporation.emitted_ticker)
 
+    logger.info(f"Saving event: {event}")
     repo.save(event)
 
 
 def add_group_corporate_event(subject: str, group: GroupEvent, repo: ManualEarningInAssetsRepository):
     if group.grouping_factor >= 1:
+        logger.info(f"Invalid grouping factor: {group.grouping_factor}")
         raise InvalidGroupingFactorError("O fator de grupamento não pode ser maior ou igual a 1.")
     _validate_last_date_prior(group.last_date_prior)
 
@@ -59,11 +67,13 @@ def add_group_corporate_event(subject: str, group: GroupEvent, repo: ManualEarni
                                                  last_date_prior=group.last_date_prior,
                                                  grouping_factor=group.grouping_factor,
                                                  emitted_ticker=group.ticker)
+    logger.info(f"Saving event: {event}")
     repo.save(event)
 
 
 def add_split_corporate_event(subject: str, split: SplitEvent, repo: ManualEarningInAssetsRepository):
     if split.grouping_factor <= 1:
+        logger.info(f"Invalid grouping factor: {split.grouping_factor}")
         raise InvalidGroupingFactorError("O fator de grupamento não pode ser maior ou igual a 1.")
     _validate_last_date_prior(split.last_date_prior)
 
@@ -74,4 +84,5 @@ def add_split_corporate_event(subject: str, split: SplitEvent, repo: ManualEarni
                                                  last_date_prior=split.last_date_prior,
                                                  grouping_factor=split.grouping_factor,
                                                  emitted_ticker=split.ticker)
+    logger.info(f"Saving event: {event}")
     repo.save(event)
