5699eaecab598d2611e8ed82e89c576eb364ae91
Victor Corte <victorcortelc@gmail.com>
2022-01-25 08:44:35 -0300

    lambda name and asdicting faq response

--- backend/help-api/src/adapters/inbound/api_gateway_handlers.py
+++ backend/help-api/src/adapters/inbound/api_gateway_handlers.py
@@ -1,9 +1,11 @@
+from dataclasses import asdict
 from http import HTTPStatus
 
 from aws_lambda_powertools import Logger, Tracer
+
+import core.faq as faq
 import goatcommons.utils.aws as awsutils
 import goatcommons.utils.json as jsonutils
-import core.faq as faq
 from adapters.outbound.dynamo_faq_repository import DynamoFaqRepository
 
 logger = Logger()
@@ -16,4 +18,5 @@ def get_faq_handler(event, context):
     topic = awsutils.get_query_param(event, "topic").upper()
     data = faq.get_faq(topic, DynamoFaqRepository())
 
-    return {"statusCode": HTTPStatus.OK, "body": jsonutils.dump(data)}
+    return {"statusCode": HTTPStatus.OK,
+            "body": jsonutils.dump(list(map(lambda f: asdict(f), data))) if type(data) == list else asdict(data)}
