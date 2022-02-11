91beeda27fcb632881f2406da8b7b8aa2da4c2f9
Victor Corte <victorcortelc@gmail.com>
2022-02-11 14:56:44 -0300

    queue permissions and adapters init

--- backend/friends-api/src/adapters/in/api_gateway_handlers.py
+++ backend/friends-api/src/adapters/in/api_gateway_handlers.py
@@ -1,5 +1,7 @@
+from adapters.out.cognito_user_info_adaptor import CognitoUserInfoAdapter
+from adapters.out.sqs_friend_request_publisher import SQSFriendRequestPublisher
 from application.models.user import User
-from core import add_friend
+from core import publish_friend_request
 from goatcommons.utils import json as jsonutils
 
 
@@ -8,8 +10,11 @@ def parse_user_from_event(event: dict) -> User:
     return User(sub=claims["sub"], name=claims["given_name"], email=claims["email"])
 
 
-def add_friend_handler(event, context):
+def new_friend_request_handler(event, context):
     from_user = parse_user_from_event(event)
     body = jsonutils.load(event["body"])
     to_email = body["email"]
-    add_friend.publish_friend_request(from_user, to_email)
+    user_info = CognitoUserInfoAdapter()
+    publisher = SQSFriendRequestPublisher()
+
+    publish_friend_request.publish_friend_request(from_user, to_email, user_info, publisher)
