2f054b235986d21d70f656048b561b14978666ba
Victor Corte <victorcortelc@gmail.com>
2022-02-12 10:19:12 -0300

    friend request listener

---
+++ backend/friends-api/src/core/friend_request_handler.py
@@ -0,0 +1,19 @@
+from application.models.friend import FriendRequest, RequestType, FriendsList
+from application.ports.friend_list_repository import FriendsListRepository
+
+
+def friend_request_handler(
+        request: FriendRequest,
+        repository: FriendsListRepository
+):
+    friends_list = None
+    if request.type_ == RequestType.FROM:
+        friends_list = repository.find_by_subject(request.from_.sub) or FriendsList(request.from_.sub)
+        friends_list.add_friend_invite(request.to)
+    elif request.type_ == RequestType.TO:
+        friends_list = repository.find_by_subject(request.to.sub) or FriendsList(request.to.sub)
+        friends_list.add_friend_request(request.from_)
+        # SEND TO PUSH NOTIFICATIONS
+
+    if friends_list:
+        repository.save(friends_list)
