7d87d15bb7a1dd86eaa1da3e3b0ced7bb5d4456b
Victor Corte <victorcortelc@gmail.com>
2022-02-14 08:56:50 -0300

    todo

---
+++ backend/friends-api/src/application/ports/friend_request_response.py
@@ -0,0 +1,5 @@
+def friend_request_response(subject: str, ):
+    # CRIAR UM ENUM OU COISA DO TIPO
+    # ACCEPT OU DECLINE
+    # VER SE O SUBJECT DO AUTHORIZATION BATE COM O SUBJECT DO request TO
+    pass
