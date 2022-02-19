ee1bcbf8db786489f25c58c1c3d5d77b03889429
Victor Corte <victorcortelc@gmail.com>
2022-02-19 06:06:15 -0300

    accept requests handlres

--- backend/friends-api/src/core/friend_request_handler.py
+++ backend/friends-api/src/core/friend_request_handler.py
@@ -1,11 +1,14 @@
 from typing import Callable, Dict
 
+from aws_lambda_powertools import Logger
+
 from application.models.friend import FriendRequest, RequestType, FriendsList
 from application.ports.friend_list_repository import FriendsListRepository
 from goatcommons.notifications.client import PushNotificationsClient
 from goatcommons.notifications.models import NotificationRequest
 
 RequestTypeHandler = Callable[[FriendRequest, FriendsListRepository, PushNotificationsClient], FriendsList]
+logger = Logger()
 
 
 def request_from_type_handler(request: FriendRequest, repository: FriendsListRepository, _) -> FriendsList:
@@ -33,14 +36,35 @@ def request_to_type_handler(request: FriendRequest,
 
 def request_accept_from_type_handler(request: FriendRequest,
                                      repository: FriendsListRepository,
-                                     push_client: PushNotificationsClient) -> FriendsList:
-    ...
+                                     _) -> FriendsList:
+    from_list = repository.find_by_subject(request.from_.sub)
+
+    if from_list.user_exists_on_requests(request.to):
+        from_list.accept_request(request.to)
+    else:
+        logger.info(f"Invalid request, user not exists on requests list. from_list={from_list}")
+
+    return from_list
 
 
 def request_accept_to_type_handler(request: FriendRequest,
                                    repository: FriendsListRepository,
                                    push_client: PushNotificationsClient) -> FriendsList:
-    ...
+    to_list = repository.find_by_subject(request.to.sub)
+
+    if to_list.user_exists_on_invites(request.from_):
+        to_list.invite_accepted(request.from_)
+        push_client.send(
+            NotificationRequest(
+                subject=request.to.sub,
+                title=f"{request.from_.name} aceitou seu convite.",
+                message=f"Venha conferir."
+            )
+        )
+    else:
+        logger.info(f"Invalid request, user not exists on invites list. to_list={to_list}")
+
+    return to_list
 
 
 def request_decline_from_type_handler(request: FriendRequest,
