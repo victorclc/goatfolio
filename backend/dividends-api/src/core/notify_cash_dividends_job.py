0c07234fca3b2c08503b982016c83befcae18384
Victor Corte <victorcortelc@gmail.com>
2022-05-05 09:03:46 -0300

    refactor dividends calculation and listener relationed stuff

--- backend/dividends-api/src/core/notify_cash_dividends_job.py
+++ backend/dividends-api/src/core/notify_cash_dividends_job.py
@@ -1,8 +1,6 @@
 import datetime
 import json
 import locale
-from decimal import Decimal
-from itertools import groupby
 from typing import List, Protocol
 
 from aws_lambda_powertools import Logger, Metrics
@@ -12,7 +10,7 @@ from adapters.outbound.dynamo_investments_repository import DynamoInvestmentRepo
 from adapters.outbound.rest_corporate_events_client import RESTCorporateEventsClient
 from adapters.outbound.rest_ticker_info_client import RestTickerInfoClient
 from application.models.dividends import CashDividends
-from application.models.invesments import StockInvestment, OperationType
+from core.helpers import TickerInfoClient, CashDividendsEarningsHelper
 from goatcommons.notifications.client import PushNotificationsClient
 from goatcommons.notifications.models import NotificationRequest
 
@@ -30,25 +28,6 @@ class CorporateEventsClient(Protocol):
         ...
 
 
-class TickerInfoClient(Protocol):
-    def get_ticker_from_isin_code(self, isin_code: str) -> str:
-        ...
-
-
-def calculate_amount(investments: List[StockInvestment]) -> Decimal:
-    amount = Decimal(0)
-    for inv in investments:
-        if inv.operation in [
-            OperationType.BUY,
-            OperationType.SPLIT,
-            OperationType.INCORP_ADD,
-        ]:
-            amount = amount + inv.amount
-        else:
-            amount = amount - inv.amount
-    return amount
-
-
 def notify_cash_dividends_job(
         processing_date: datetime.date,
         investments_repository: DynamoInvestmentRepository,
@@ -59,33 +38,27 @@ def notify_cash_dividends_job(
     logger.info(f"Notify cash dividends job - START: {processing_date.strftime('%Y-%m-%d')}")
 
     cash_dividends = corporate_events_client.get_cash_dividends(processing_date)
+    helper = CashDividendsEarningsHelper(corporate_events_client, ticker_info_client, investments_repository)
+
     for dividend in cash_dividends:
         logger.info(f"Processing dividend: {dividend}")
-        all_isin_symbols = corporate_events_client.get_all_previous_symbols(dividend.asset_issued) \
-                           + [dividend.asset_issued]
-        tickers = list(
-            filter(lambda i: i, map(lambda i: ticker_info_client.get_ticker_from_isin_code(i), all_isin_symbols))
-        )
-        logger.info(f"Tickers: {tickers}")
+        payouts = helper.calculate_earnings_of_cash_dividend_for_all_users(dividend)
 
-        investments = []
-        for ticker in tickers:
-            investments += investments_repository.find_by_ticker_until_date(ticker, processing_date)
-        logger.info(f"Found {len(investments)} applicable investments.")
+        for payout in payouts:
+            subject = payout["subject"]
+            ticker = payout["ticker"]
+            payed_amount = payout["payed_amount"]
 
-        for subject, sub_investments in groupby(sorted(investments, key=lambda i: i.subject), key=lambda i: i.subject):
-            amount = calculate_amount(list(sub_investments))
-            if amount > 0:
-                logger.info(f"Sending push for {subject}.")
-
-                push_client.send(
-                    NotificationRequest(
-                        subject=subject,
-                        title="Tem dinheiro entrando! 🐐🎉",
-                        message=f"Hoje, você vai receber R$ {locale.currency(dividend.rate * amount, grouping=True, symbol=False)} de rendimentos da ação {tickers[-1]}"
-                    )
+            logger.info(f"Sending push for {subject}.")
+            push_client.send(
+                NotificationRequest(
+                    subject=subject,
+                    title="Tem dinheiro entrando! 🐐🎉",
+                    message=f"Hoje, você vai receber R$ {locale.currency(payed_amount, grouping=True, symbol=False)} de rendimentos da ação {ticker}"
                 )
-                metrics.add_metric(name="DividendNotifiedCount", unit=MetricUnit.Count, value=1)
+            )
+            metrics.add_metric(name="DividendNotifiedCount", unit=MetricUnit.Count, value=1)
+
     metrics.add_metric(name="ApplicableCashDividendsCount", unit=MetricUnit.Count, value=len(cash_dividends))
     dump_metrics()
     logger.info(f"Notify cash dividends job - END")
